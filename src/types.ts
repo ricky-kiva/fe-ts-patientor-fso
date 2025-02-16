87c1c6e11c2912dd2eb85514757956f612ca2c0c
ricky-kiva <ricky.kiva@gmail.com>
2025-02-17 01:16:23 +0700

    feat(types): add `Entry` type

--- src/types.ts
+++ src/types.ts
@@ -10,6 +10,49 @@ export enum Gender {
   Other = "other"
 }
 
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HospitalDischarge {
+  date: string,
+  criteria: string
+}
+
+interface OccupationalHealthcareSickLeave {
+  startDate: string,
+  endDate: string
+}
+
+interface BaseEntry {
+  id: string,
+  description: string,
+  date: string,
+  specialist: string,
+  diagnosisCodes?: Array<Diagnosis['code']>
+}
+
+interface HospitalEntry extends BaseEntry {
+  discharge: HospitalDischarge,
+  type: "Hospital"
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  employerName: string,
+  sickLeave?: OccupationalHealthcareSickLeave,
+  type: "OccupationalHealthcare"
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  healthCheckRating: HealthCheckRating,
+  type: "HealthCheck"
+}
+
+export type Entry = HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry;
+
 export interface Patient {
   id: string;
   name: string;
@@ -17,6 +60,7 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[]
 }
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
