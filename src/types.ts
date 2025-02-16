21fbccb6613466d4afb6bc1eb44a4c7f4ba9e60d
ricky-kiva <ricky.kiva@gmail.com>
2025-02-16 21:22:58 +0700

    feat(types): add `assertNever` func. to ensure enum exhaust. switch

--- src/types.ts
+++ src/types.ts
@@ -20,3 +20,7 @@ export interface Patient {
 }
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
+
+export const assertNever = (value: never): never => {
+  throw new Error(`Unhandled discriminated union member: ${JSON.stringify(value)}`);
+};
